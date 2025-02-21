334b48b2786a3241e34712ae3886753a4798d923
AlyLongEgg <rainbowmarks7@gmail.com>
2025-02-21 23:59:12 +0000

    I think I did this right?

--- Main.java
+++ Main.java
@@ -7,10 +7,23 @@ public class Main
 	public static void main(String[] args)
 	{
     //DECLARATION + INITIALIZATION SECTION
-
+    double initialAmount = 3.17;
+    int centsRemaining = 0;
+    int Quarters = 0;
+    int Dimes = 0;
+    int Nickles = 0;
+    int Pennies = 0;
     //CALCULATION SECTION
-
-    //OUTPUT SECTION
+    centsRemaining = (int)(100*initialAmount);
+    Quarters = (int)(centsRemaining/25);
+    centsRemaining = centsRemaining%25;
+    Dimes = centsRemaining/10;
+    centsRemaining = centsRemaining%10;
+    Nickles = centsRemaining/5;
+    centsRemaining = centsRemaining%5;
+    Pennies = centsRemaining/1;
+      //OUTPUT SECTION
+  System.out.println("$" +initialAmount+ " can be converted to " +Quarters+ " quarters, " +Dimes+ " dimes, " +Nickles+ " nickles, and " +Pennies+ " pennies.");
 
   }
 
